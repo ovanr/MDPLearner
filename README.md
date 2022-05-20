28ad74a71f01184ee3f77c62022d3a0ad2677d8e
Orpheas van Rooij <orpheas.vanrooij@outlook.com>
2022-05-20 21:14:36 +0200

    readme added

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## MDP-Learner
+
+
+### Example Usage
+
+```
+    python main.py -v models/ex1.prism
+```
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 
 from sys import argv
 
-from MDPLearner.logging import set_up_logging, DEBUG
+from MDPLearner.logging import set_up_logging, INFO, WARNING
 from MDPLearner.frequentist import FrequentistLearner
 from MDPLearner.simulator import Simulator
 from MDPLearner.model import Model
 
+logging_mode = WARNING
 
-if len(argv) <= 1:
-    raise Exception("Enter path of prism file")
+if len(argv) == 3 and argv[1] == "-v":
+    logging_mode = INFO
+    path = argv[2]
+elif len(argv) == 2:
+    path = argv[1]
+else:
+    raise Exception(f"Usage: {argv[0]} [-v] FILE")
 
-set_up_logging(DEBUG)
+set_up_logging(logging_mode)
 
-model = Model(argv[1])
+model = Model(path)
 simulator = Simulator(model)
 
 simulator.mk_observations()
