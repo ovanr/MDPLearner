bf7dfe929da7e43f5fbc60d7dced9b6c32c86289
Orpheas van Rooij <orpheas.vanrooij@outlook.com>
2022-06-28 07:24:00 -0700

    add plotter generator scripts

--- inc_n.sh
+++ inc_n.sh
@@ -15,9 +15,10 @@ out_file=$(basename `echo $model | cut -d '.' -f 1`.dat)
 echo '# n actual freq dirichlet' &> $out_file
 
 for n in 200 500 1000 2000 4000 6000 8000 10000; do
+    rm out/*
     python main.py -N $n --batches 1 $model &> /dev/null
     actual=`runStorm out/model.prism`
     freq=`runStorm out/frequentist_model.prism`
-    dirichlet=`runStorm out/dirichlet_model_0.prism`
+    dirichlet=`runStorm out/dirichlet_model_*.prism`
     echo $n $actual $freq $dirichlet &>> $out_file
 done
--- incremental.sh
+++ incremental.sh
@@ -14,12 +14,13 @@ out_file=$(basename `echo $model | cut -d '.' -f 1`.dat)
 
 echo '# n actual freq dirichlet' &> $out_file
 
+rm out/*
 python main.py -N 3000 --batches 15 $model &> /dev/null
+actual=`runStorm out/model.prism`
+freq=`runStorm out/frequentist_model.prism`
 
-for file in `ls out/dirichlet_model*.prism | sort`; do
-    actual=`runStorm out/model.prism`
-    freq=`runStorm out/frequentist_model.prism`
-    dirichlet=`runStorm out/${file}.prism`
+for file in `ls out/dirichlet_model*.prism | sort -t '_' -k3n`; do
+    dirichlet=`runStorm ${file}`
     batchSize=`echo $file | cut -d '_' -f 3 | cut -d '.' -f 1`
     echo $batchSize $actual $freq $dirichlet &>> $out_file
 done
